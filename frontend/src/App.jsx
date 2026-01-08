a56aa11e3ee79aee1498a1408ae2a6d736c4e8ea
sapanajoshi140-ux <sapanajoshi140@gmail.com>
2026-01-08 19:27:03 +0545

    Added hero section with slides

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import Navbar from "./Navbar.jsx"
+import Picture from "./HeroSlider.jsx"
 function App(){
     return(
+        <div className="min-h-screen px-6 pt-4">
         <Navbar />
+        <Picture />
+     
+        </div>
     )
 }
 export default App;

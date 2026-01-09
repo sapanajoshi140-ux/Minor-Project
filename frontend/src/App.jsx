26e6877ba2e5ebc7377e41ea2430111e1b68de39
sapanajoshi140-ux <sapanajoshi140@gmail.com>
2026-01-09 20:49:45 +0545

    Added Feature section and animation in both section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./Navbar.jsx"
 import Picture from "./HeroSlider.jsx"
+import Feature from "./Features.jsx";
 function App(){
     return(
-        <div className="min-h-screen px-6 pt-4">
+        <div className="min-h-screen px-0 pt-4">
         <Navbar />
         <Picture />
+        <Feature />
      
         </div>
     )
